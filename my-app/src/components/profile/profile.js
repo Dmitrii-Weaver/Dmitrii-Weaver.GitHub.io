07d4ef0035147527601a3cc3cc39b568c006e8ae
Weaver <cordies1106@gmail.com>
2023-10-20 04:01:35 +0300

    Timeline content added

--- my-app/src/components/profile/profile.js
+++ my-app/src/components/profile/profile.js
@@ -1,24 +1,51 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import CustomTimeline from "../timeline/timeline"
+import CustomTimeline, { CustomTimelineSeparator } from "../timeline/timeline"
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import resumeData from "../../utils/resumeData";
 import './profile.css'
+import { TimelineContent, TimelineItem } from "@mui/lab";
+
+
+const CustomTimelineItem = ({ title, text, link }) => (
+    <TimelineItem>
+        <CustomTimelineSeparator />
+        <TimelineContent className="timeline_content">
+            {link ? (
+                <Typography className="timeline_text"><span>{title}:</span> <a href={link} target="_blank">{text}</a></Typography>
+            ) : (
+                <Typography className="timeline_text"><span>{title}:</span> {text}</Typography>
+            )}
+        </TimelineContent>
+
+    </TimelineItem>
+);
+
 
 const Profile = () => {
     return (
         <div className="profile container_shadow">
             <div className="profile_name">
-                <Typography className="name">Dmitrii</Typography>
-                <Typography className="title">Dmitrii</Typography>
+                <Typography className="name">{resumeData.name}</Typography>
+                <Typography className="title">{resumeData.title}</Typography>
             </div>
             <figure className="profile_image">
-                <img src={require("../../assets/images/display.jpg")} alt=""/>
+                <img src={require("../../assets/images/display.jpg")} alt="" />
             </figure>
             <div className="profile_info">
-                <CustomTimeline />
-                <br/>
+                <CustomTimeline icon={<PersonOutlineIcon />} >
+                    <CustomTimelineItem title={"Name"} text={resumeData.name}/>
+                    <CustomTimelineItem title={"DOB"} text={resumeData.birthday}/>
+                    <CustomTimelineItem title={"Occupation"} text={resumeData.job}/>
+                    <CustomTimelineItem title={"Email"} text={resumeData.email}/>
+                    {Object.keys(resumeData.socials).map((key) => (
+                        <CustomTimelineItem title={key} text={resumeData.socials[key].text} link={resumeData.socials[key].link}/>
+                    ))}
+                </CustomTimeline>
+                <br />
                 <button>123</button>
             </div>
-        </div> 
+        </div>
     )
 }
 
---
+++ my-app/src/utils/resumeData.js
@@ -0,0 +1,28 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export default {
+    name: "Dmitrii Tkachev",
+    title: "Fullstack developer",
+    job: "Freelancer",
+    birthday: "11.06.2001",
+    email: "dmitriitkachev11@gmail.com",
+    location: "Oulu, Finland",
+    phone: "+358-44-917-1626",
+
+    socials: {
+        Linkedin: {
+            link: "https://www.linkedin.com/in/dmitrii-tkachev-a65704224/",
+            text: "Linkedin",
+            icon: <LinkedInIcon/>
+        },
+        Github: {
+            link: "https://github.com/Dmitrii-Weaver",
+            text: "Github",
+            icon: <GitHubIcon/>
+        },
+
+    }
+
+
+}
