5dde152b40883ef596119d40d07e1ffa8523bfc2
Weaver <cordies1106@gmail.com>
2023-11-07 21:20:47 +0200

    footer

--- my-app/src/components/footer/footer.js
+++ my-app/src/components/footer/footer.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./footer.css"
+import { Typography } from '@mui/material'
+import resumeData from "../../utils/resumeData";
+
 
 const Footer = () => {
   return (
-    <div>
-        Footer comp
+    <div className='footer'>
+      <div className='footer_left'>
+        <Typography className='footer_name'> {resumeData.name} </Typography>
+      </div>
+      <div className='footer_right'>
+        <Typography className='footer_copy'>Designed and developed by  <a href='/' target='_blank'>Dmitrii Tkachev </a>
+          <br />
+          Original idea from  <a href='https://themeforest.net/' target='_blank' >Tavonline.net</a></Typography>
+
+      </div>
     </div>
   )
 }
