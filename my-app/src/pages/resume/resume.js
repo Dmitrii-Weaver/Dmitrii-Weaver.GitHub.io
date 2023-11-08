8da7a8b9e40ebcfb89701de7950058b88323cde1
Weaver <cordies1106@gmail.com>
2023-11-09 00:10:04 +0200

    education and work timeline added

--- my-app/src/pages/resume/resume.js
+++ my-app/src/pages/resume/resume.js
@@ -2,6 +2,10 @@ import React from 'react'
 import "./resume.css"
 import { Grid, Typography } from '@mui/material'
 import resumeData from "../../utils/resumeData";
+import CustomTimeline, { CustomTimelineSeparator } from "../../components/timeline/timeline.js"
+import WorkIcon from '@mui/icons-material/Work';
+import { TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@mui/lab';
+import SchoolIcon from '@mui/icons-material/School';
 
 const Resume = () => {
   return (
@@ -10,15 +14,56 @@ const Resume = () => {
       <Grid container className='section p_45'>
         <Grid item className='section_title mb_30' >
           <span></span>
-          <h6 className='section_title_text'>About Me:</h6>
+          <h6 className='section_title_text'>About Me</h6>
         </Grid>
         <Grid item className='' xs={12}>
-          <Typography variant='body2'  className='about_me'>{resumeData.about}</Typography>
+          <Typography variant='body2' className='about_me'>{resumeData.about}</Typography>
         </Grid>
       </Grid>
 
       {/*education*/}
-      <Grid container className='section'></Grid>
+      <Grid container className='section top_30'>
+        <Grid item className='section_title mb_30' >
+          <span></span>
+          <h6 className='section_title_text'>Resume</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container className='resume_timeline'>
+            {/*exp*/}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Working history" icon={<WorkIcon />} >
+                {resumeData.workexp.map(e => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className='timeline_content'>
+                      <Typography className='timeline_title'>{e.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{e.date}</Typography>
+                      <Typography variant="body2" className='timeline_desc'>{e.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+
+            {/*edu*/}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title="Education" icon={<SchoolIcon />} >
+                {resumeData.education.map(e => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className='timeline_content'>
+                      <Typography className='timeline_title'>{e.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>{e.date}</Typography>
+                      <Typography variant="body2" className='timeline_desc'>{e.description}</Typography>
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
 
       {/*services*/}
       <Grid container className='section'></Grid>
--- my-app/src/utils/resumeData.js
+++ my-app/src/utils/resumeData.js
@@ -14,17 +14,47 @@ export default {
         Linkedin: {
             link: "https://www.linkedin.com/in/dmitrii-tkachev-a65704224/",
             text: "Linkedin",
-            icon: <LinkedInIcon/>
+            icon: <LinkedInIcon />
         },
         Github: {
             link: "https://github.com/Dmitrii-Weaver",
             text: "Github",
-            icon: <GitHubIcon/>
+            icon: <GitHubIcon />
         },
 
     },
 
-    about : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    about: "Fresh OAMK graduate - Fullstack Developer and Web Designer with main focus on Web development using React, CSS and Typescript with additional skills and experience in Python, C#, React Native, PHP and Kotlin.  \n  \n Completed full Bachelors Degree in OAMK in the field od Computer Sciences and additional exchange studies in Kitami, Japan in the same field. \n  \n Currently looking for an entry-level position in an IT company on place or remote",
 
 
+    workexp: [
+        {
+            title: "Backend developer C# .Net - Keyloop",
+            date: "2022.02 - 2022.08",
+            description: "Designing, developing and testing new functions for company's API on C# .Net while also supervising a group of two other students as a part of my university studies.",
+        },
+        {
+            title: "Frontend developer - Cavokapp.com",
+            date: "2021.05 - 2021.08",
+            description: "Implementing new functions for the service's web page.",
+        },
+        {
+            title: "Survey engineer - Terra Pulse Group",
+            date: "2018 - 2021",
+            description: 'Creating online surveys using the documentation provided by client via Alchemer tools.',
+        }
+    ],
+
+    education: [
+        {
+            title: "OAMK - Bachelor's degree in computer sciences",
+            date: "2019-2023",
+            description: "Graduated as Software Engineer with GPA of 4.5 in August 2023",
+        },
+        {
+            title: "KIT - Kitami Institute of Technology. Japan.",
+            date: "2022.10-2023.03",
+            description: "Completed exchange studies in a university in Japan in the field of computer sciences",
+        },
+    ]
 }
