db2d31b575ae85e56c65c730c5c36bf470250647
Weaver <cordies1106@gmail.com>
2023-11-10 00:48:56 +0200

    services added

--- my-app/src/pages/resume/resume.js
+++ my-app/src/pages/resume/resume.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./resume.css"
-import { Grid, Typography } from '@mui/material'
+import { Grid, Icon, Paper, Typography } from '@mui/material'
 import resumeData from "../../utils/resumeData";
 import CustomTimeline, { CustomTimelineSeparator } from "../../components/timeline/timeline.js"
 import WorkIcon from '@mui/icons-material/Work';
@@ -11,7 +11,7 @@ const Resume = () => {
   return (
     <>
       {/*about me*/}
-      <Grid container className='section p_45'>
+      <Grid container className='section pb_45 pt_45'>
         <Grid item className='section_title mb_30' >
           <span></span>
           <h6 className='section_title_text'>About Me</h6>
@@ -22,7 +22,7 @@ const Resume = () => {
       </Grid>
 
       {/*education*/}
-      <Grid container className='section top_30'>
+      <Grid container className='section pb_45'>
         <Grid item className='section_title mb_30' >
           <span></span>
           <h6 className='section_title_text'>Resume</h6>
@@ -66,10 +66,49 @@ const Resume = () => {
       </Grid>
 
       {/*services*/}
-      <Grid container className='section'></Grid>
+      <Grid container className='section pb_45'>
+        <Grid item className='section_title mb_30' >
+          <span></span>
+          <h6 className='section_title_text'>Services</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={3} justifyContent='space-around'>
+            {resumeData.services.map(service => (
+              <Grid item xs={12} sm={6} md={3}>
+                <div className='service'>
+                  <Icon className='service_icon'>{service.icon}</Icon>
+                  <Typography className='service_title' variant='h6'>{service.title}</Typography>
+                  <Typography className='service_desc' variant='body2'>{service.description}</Typography>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+
+        </Grid>
+      </Grid>
 
       {/*skills*/}
-      <Grid container className='section'></Grid>
+      <Grid container  className='section graybg pb_45 p_50'>
+        <Grid item xs={12}>
+          <Grid container justifyContent='space-between' spacing={3}>
+            {resumeData.skills.map(skill => (
+              <Grid item xs={12} sm={6} md={3}>
+                <Paper elevation={0} className='skill'>
+                  <Typography variant='h6' className='skill_title'>{skill.title}</Typography>
+                  {skill.description.map(desc => (
+                    <Typography variant='body2' className='skill_desc'>
+                      <TimelineDot variant={'outlined'} className='tl_dot' />{desc}
+                    </Typography>
+                  ))}
+                </Paper>
+              </Grid>
+            ))}
+
+          </Grid>
+        </Grid>
+
+
+      </Grid>
 
       {/*contacts*/}
       <Grid container className='section'></Grid>
--- my-app/src/utils/resumeData.js
+++ my-app/src/utils/resumeData.js
@@ -1,5 +1,8 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import WebIcon from '@mui/icons-material/Web';
+import StorageIcon from '@mui/icons-material/Storage';
+import BrushIcon from '@mui/icons-material/Brush';
 
 export default {
     name: "Dmitrii Tkachev",
@@ -55,6 +58,43 @@ export default {
             title: "KIT - Kitami Institute of Technology. Japan.",
             date: "2022.10-2023.03",
             description: "Completed exchange studies in a university in Japan in the field of computer sciences",
+        },
+    ],
+
+    services: [
+        {
+            title: "Web developer",
+            description: "Design, implementation and assembly of web applications. 5 years of experience + Bachelor's degree.",
+            icon: <WebIcon />
+        },
+        {
+            title: "Back-end engineer",
+            description: "Development of backend solutions, interactions with Databases, request management. 3 years of experience + Bachelor's degree.",
+            icon: <StorageIcon />
+        },
+        {
+            title: "Illustrator",
+            description: "Decent skill in graphics and character design design. 2 Years of experience on a hobby level.",
+            icon: <BrushIcon />
+        },
+    ],
+
+    skills: [
+        {
+            title: "Front-end",
+            description: ["React", "CSS", "Bootstrap", "Material UI", "Typescript", "React Native",]
+        },
+        {
+            title: "Back-end",
+            description: ["C# .NET", "Python", "Node JS", "Firebase", "MySQL"]
+        },
+        {
+            title: "Source Control",
+            description: ["Git", "Github", "SCRUM/Agile",]
+        },
+        {
+            title: "Design",
+            description: ["SAI2", "Shotcut"]
         },
     ]
 }
